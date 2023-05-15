b077d358ae55b38d131871d7dc4c930db66bb5b8
mmartijnNL <martijn_kalteren@hotmail.com>
2023-05-15 11:40:38 +0200

    Added iption to ignore dst

--- VersionFormat/TemplateHandler.cs
+++ VersionFormat/TemplateHandler.cs
@@ -2,6 +2,10 @@
 {
     public class TemplateHandler
     {
+        /// <summary>
+        /// Generates a version based on the given template. Uses DateTime.ToString to format datetimes (e.g. "{yyyy}" -> "2023").
+        /// You choose to ignore the the daylight savings by adding -DST (e.g. "{-DSTHH:mm}" -> "08:42").
+        /// </summary>
         public static string GetVersionFromTemplate(string template)
         {
             string version = template;
@@ -16,10 +20,19 @@
 
             // Resolve all other sections using the date time now
             var now = DateTime.Now;
+            var nowIgnoreDaylightSavings = DateTime.Now.ToUniversalTime() + TimeZoneInfo.Local.BaseUtcOffset;
             foreach (var section in sections)
             {
                 var trimmedSection = section.Replace("{", "").Replace("}", "");
-                version = version.Replace(section, now.ToString(trimmedSection));
+                if (trimmedSection.StartsWith("-DST"))
+                {
+                    trimmedSection = trimmedSection.Substring(4);
+                    version = version.Replace(section, nowIgnoreDaylightSavings.ToString(trimmedSection));
+                }
+                else
+                {
+                    version = version.Replace(section, now.ToString(trimmedSection));
+                }
             }
 
             return version;
